ba6ace7639a2e5fe6323ed995ecca5ef8f94ab90
Prateek Saxena <prtksxna@gmail.com>
2014-06-30 17:53:17 +0530

    Use attributes to initialize the map

--- leaflet-map-component.js
+++ leaflet-map-component.js
@@ -1,10 +1,22 @@
 Polymer( 'leaflet-map', {
+
+    // Setting Defaults
+    latitude: 15.466736,
+    longitude: 73.808159,
+    zoom: 10,
+    tileServer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    zoomControls: true,
+    staticmap: false,
+
     ready: function () {
-        var baseLayer = L.tileLayer( 'http://{s}.tile.osm.org/{z}/{x}/{y}.png');
-        var map = new L.Map( this.$.map , {
-                center: new L.LatLng(51.505, -0.09),
-                zoom: 12,
+        var baseLayer = L.tileLayer( this.tileServer );
+        var lat = this.latitude;
+        var lon = this.longitude;
+        var zoom = this.zoom;
+        this.map = new L.Map( this.$.map , {
+            center: new L.LatLng( this.latitude, this.longitude ),
+            zoom: this.zoom,
             layers: [baseLayer]
-        });
+        } );
     }
 } );
