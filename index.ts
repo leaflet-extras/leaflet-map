112cd90102a17a6a72b3d8b132f2fa53d430a016
Benny Powers <web@bennypowers.com>
2020-12-06 21:26:08 +0200

    feat: leaflet-legend

--- index.ts
+++ index.ts
@@ -1,29 +1,42 @@
 import './leaflet-map';
+import './leaflet-layer-group';
+
 import './leaflet-polygon';
 import './leaflet-polyline';
 import './leaflet-circle';
 import './leaflet-point';
-import './leaflet-control';
-import './leaflet-tilelayer';
-import './leaflet-tilelayer-wms';
-import './leaflet-layer-group';
+
 import './leaflet-geojson';
 import './leaflet-marker';
 import './leaflet-icon';
 import './leaflet-divicon';
 import './leaflet-geolocation';
 
+import './leaflet-scale-control';
+import './leaflet-zoom-control';
+import './leaflet-fullscreen-control';
+import './leaflet-legend';
+
+import './leaflet-tilelayer';
+import './leaflet-tilelayer-wms';
+
 export * from './leaflet-map';
+export * from './leaflet-layer-group';
+
 export * from './leaflet-polygon';
 export * from './leaflet-polyline';
 export * from './leaflet-circle';
 export * from './leaflet-point';
-export * from './leaflet-control';
-export * from './leaflet-tilelayer';
-export * from './leaflet-tilelayer-wms';
-export * from './leaflet-layer-group';
 export * from './leaflet-geojson';
 export * from './leaflet-marker';
 export * from './leaflet-icon';
 export * from './leaflet-divicon';
 export * from './leaflet-geolocation';
+
+export * from './leaflet-scale-control';
+export * from './leaflet-zoom-control';
+export * from './leaflet-fullscreen-control';
+export * from './leaflet-legend';
+
+export * from './leaflet-tilelayer';
+export * from './leaflet-tilelayer-wms';
